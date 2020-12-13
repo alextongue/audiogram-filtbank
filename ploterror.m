45ea6f36b554bceefe600b5b5d5b8859d65738e9
Alex Tung <atung323@gmail.com>
2020-12-13 13:08:06 -0800

    fix to correctly show error=query/ref, or 'Y/X'

--- ploterror.m
+++ ploterror.m
@@ -5,6 +5,7 @@ function fhandleout = ploterror(ref_freqs, ref_mag, query_ir, fs, fhandlein)
         fhandleout = figure(fhandlein);
     end
     
+    logplot = true; % (! HARD CODED FOR NOW !)
     fft_len = 2^ceil(log2(numel(query_ir)));
     
     % interpolate
@@ -12,9 +13,9 @@ function fhandleout = ploterror(ref_freqs, ref_mag, query_ir, fs, fhandlein)
     query_mag = 20*log10(abs(query_tf));
     fftfreqs = (0:(fft_len-1)).*fs/fft_len;
     
-    ref_mag_interp = interp1(ref_freqs,ref_mag,fftfreqs);
+    ref_mag_interp = interp1(ref_freqs,ref_mag,fftfreqs,[],'extrap');
     
-    error_tf = ref_mag_interp - query_mag;
+    error_tf = query_mag - ref_mag_interp;
     
     subplot(2,1,1);
     plot(fftfreqs,ref_mag_interp,'k',...
@@ -23,9 +24,12 @@ function fhandleout = ploterror(ref_freqs, ref_mag, query_ir, fs, fhandlein)
     plot(fftfreqs,query_mag,'k:',...
         'linewidth',1.5,'displayname','after optimization');
     hold off;
+    if logplot
+        set(gca,'XScale','log');
+    end
     xlim([20,fs/2]);
     grid on;
-    legend show;
+    legend('location','best');
     
     subplot(2,1,2);
     plot(fftfreqs,zeros(size(fftfreqs)),'k',...
@@ -34,9 +38,13 @@ function fhandleout = ploterror(ref_freqs, ref_mag, query_ir, fs, fhandlein)
     plot(fftfreqs,error_tf,'k:',...
         'linewidth',1.5,'displayname','after optimization')
     hold off;
+    if logplot
+        set(gca,'XScale','log');
+    end
     xlim([20,fs/2])
+    ylim([-10,10]);
     grid on;
-    legend show;
+    legend('location','best');
     title('dB Error');
     
     
